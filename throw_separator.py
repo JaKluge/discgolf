7d9c7bb78b75fd2902f0eec25935e7ada60857e8
Kevin Klein <kevin.klein@student.hpi.de>
2024-05-05 12:20:19 +0200

    use combined acceleration vector

--- throw_separator.py
+++ throw_separator.py
@@ -1,22 +1,21 @@
 import os
 import pandas as pd
 import numpy as np
-import shutil
+#import shutil
 
-def find_maxima_of_throws(data, window_size=30, backhand=True):
-    if backhand:
-        acc_x_threshold = 120
-    else:
-        acc_x_threshold = 40
-    
-    above_threshold_positions = data[data['FreeAcc_X'] > acc_x_threshold].index
+def find_maxima_of_throws(df, window_size=30):
+    acc_threshold = 150
+
+    df['Acc_Vector'] = np.sqrt(df['FreeAcc_X'] ** 2 + df['FreeAcc_Y'] ** 2 + df['FreeAcc_Z'] ** 2)
+        
+    above_threshold_positions = df[df['Acc_Vector'] > acc_threshold].index
     max_indices = []
     
     for position in above_threshold_positions:
         start_index = max(0, position - window_size)
-        end_index = min(len(data), position + window_size)
-        window_data = data.iloc[start_index:end_index]
-        max_index = window_data['FreeAcc_X'].idxmax()
+        end_index = min(len(df), position + window_size)
+        window_df = df.iloc[start_index:end_index]
+        max_index = window_df['Acc_Vector'].idxmax()
         max_indices.append(max_index)
     
     max_indices = np.array(list(set(max_indices)))
@@ -27,10 +26,8 @@ def split_and_save_throws(input_path, output_path, time_window=25):
     files = os.listdir(input_path)
     num_metadata_lines = 11
     folder_names = ['Jannie', 'Julian', 'Kevin', 'Forehand']
-    folder_name = None
     
     for file_name in files:
-        backhand = True
         
         if not file_name.startswith('2024'):
             continue
@@ -47,25 +44,21 @@ def split_and_save_throws(input_path, output_path, time_window=25):
         for name in folder_names:
             if name in input_folder:
                 output_folder = os.path.join(output_path, name)
-                folder_name = name
                 break
       
         if not os.path.exists(output_folder):
             os.makedirs(output_folder)
 
         df = pd.read_csv(input_file, sep=',', skiprows=num_metadata_lines)
-        
-        if folder_name == "Forehand":
-            backhand = False
             
-        throw_indices = find_maxima_of_throws(df, backhand=backhand)
+        throw_indices = find_maxima_of_throws(df)
         
         for i, throw_index in enumerate(throw_indices):
-             start_time = max(0, throw_index - time_window)
-             end_time = min(len(df), throw_index + time_window)
-             throw_data = df.iloc[start_time:end_time]
-             throw_filename = os.path.join(output_folder, ts_file[:-4] + f"_{i+1}.csv")
-             throw_data.to_csv(throw_filename, index=False)
+            start_time = max(0, throw_index - time_window)
+            end_time = min(len(df), throw_index + time_window)
+            throw_data = df.iloc[start_time:end_time]
+            throw_filename = os.path.join(output_folder, ts_file[:-4] + f"_{i+1}.csv")
+            throw_data.to_csv(throw_filename, index=False)
 
 if __name__ == "__main__":
     input_path = "data/20240430"
