aae095e1983fe3ee1a6522b09ff2f34bc9b512f5
JaKluge <jannykluge@googlemail.com>
2024-06-20 09:01:46 +0200

    add knn on time series data

--- classification.py
+++ classification.py
@@ -6,7 +6,16 @@ from tsfresh.feature_extraction import extract_features
 from tsfresh.feature_extraction import settings
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from tslearn.preprocessing import TimeSeriesScalerMeanVariance
+from tslearn.neighbors import KNeighborsTimeSeriesClassifier
 from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import KFold, GridSearchCV
+
+
+# from sktime.classification.ensemble import TimeSeriesForestClassifier
+# from sklearn.pipeline import Pipeline
+# from sklearn.preprocessing import StandardScaler
 
 
 PLOT_DIR = "figures/anomaly_detection"
@@ -42,14 +51,45 @@ def feature_extraction(throw_set: np.array):
     return throw_features
 
 
-def classify(X_train, X_test, y_train, y_test):
-    clf = RandomForestClassifier(max_depth=2, random_state=0)
-    clf.fit(X_train, y_train)
+def create_ts_df(throw_list, columns):
+    n_samples = len(throw_list)
+    n_timestamps = throw_list[0].shape[0]
+    n_features = len(columns)
 
-    print(
-        "\nAccuracy of cross validation: "
-        + str(cross_val_score(clf, X_train, y_train, cv=5, scoring="accuracy"))
-    )
+    ts_data = np.empty((n_samples, n_timestamps, n_features))
+    for i, throw in enumerate(throw_list):
+        ts_data[i] = throw[columns].values
+
+    return ts_data
+
+
+def classify_ts(X_train, X_test, y_train, y_test):
+    scaler = TimeSeriesScalerMeanVariance()
+    X_train = scaler.fit_transform(X_train)
+    X_test = scaler.transform(X_test)
+
+    knn = KNeighborsTimeSeriesClassifier(n_neighbors=2)
+    p_grid = {"n_neighbors": [1, 5]}
+    cv = KFold(n_splits=5, shuffle=True, random_state=0)
+    clf = GridSearchCV(estimator=knn, param_grid=p_grid, cv=cv)
+
+    cv_scores = cross_val_score(clf, X_train, y_train, cv=cv, scoring="accuracy")
+    print("\nAccuracy of cross-validation (Time Series): ", cv_scores)
+    print("Mean cross-validation accuracy: ", np.mean(cv_scores))
+
+    clf.fit(X_train, y_train)
+    print("Predicted labels: " + str(clf.predict(X_test)))
+    print("True labels: " + str(y_test))
+
+
+def classify_features(X_train, X_test, y_train, y_test):
+    clf = RandomForestClassifier(max_depth=2, random_state=0)
+    cv_scores = cross_val_score(clf, X_train, y_train, cv=5, scoring="accuracy")
+
+    print("\nAccuracy of cross-validation (Features): ", cv_scores)
+    print("Mean cross-validation accuracy: ", np.mean(cv_scores))
+
+    clf.fit(X_train, y_train)
     print("Predicted labels: " + str(clf.predict(X_test)))
     print("True labels: " + str(y_test))
 
@@ -67,10 +107,33 @@ if __name__ == "__main__":
 
     throw_features = feature_extraction(throws_list)
 
-    X_train, X_test, y_train, y_test = train_test_split(
+    X_train_feat, X_test_feat, y_train_feat, y_test_feat = train_test_split(
         throw_features,
         labels,
         stratify=labels,
         test_size=0.2,
     )
-    classify(X_train, X_test, y_train, y_test)
+
+    classify_features(X_train_feat, X_test_feat, y_train_feat, y_test_feat)
+
+    throw_df = create_ts_df(
+        throws_list,
+        [
+            "Euler_X",
+            "Euler_Y",
+            "Euler_Z",
+            "Acc_Vector",
+            "FreeAcc_X",
+            "FreeAcc_Y",
+            "FreeAcc_Z",
+        ],
+    )
+
+    X_train_raw, X_test_raw, y_train_raw, y_test_raw = train_test_split(
+        throw_df,
+        labels,
+        stratify=labels,
+        test_size=0.2,
+    )
+
+    classify_ts(np.array(X_train_raw), np.array(X_test_raw), y_train_raw, y_test_raw)
