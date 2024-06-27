9ecfd6dff9102dab6414089f757c279cdf8f19d4
JaKluge <jannykluge@googlemail.com>
2024-06-27 09:05:03 +0200

    trying sktime

--- classification.py
+++ classification.py
@@ -11,6 +11,14 @@ from tslearn.preprocessing import TimeSeriesScalerMeanVariance
 from tslearn.neighbors import KNeighborsTimeSeriesClassifier
 from sklearn.model_selection import cross_val_score
 from sklearn.model_selection import KFold, GridSearchCV
+from sktime.classification.ensemble import BaggingClassifier
+from sktime.classification.kernel_based import RocketClassifier
+from sktime.datasets import load_unit_test
+from sktime.classification.dictionary_based import WEASEL
+from sktime.classification.shapelet_based import ShapeletTransformClassifier
+
+from sktime.classification.interval_based import TimeSeriesForestClassifier
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 
 
 # from sktime.classification.ensemble import TimeSeriesForestClassifier
@@ -20,7 +28,7 @@ from sklearn.model_selection import KFold, GridSearchCV
 
 PLOT_DIR = "figures/anomaly_detection"
 DF_DIR = "data/splitted_throws"
-SEED = np.random.RandomState(0)
+SEED = np.random.RandomState(5)
 
 
 # this takes a list of dataframes/throws and extracts features for every throw
@@ -65,22 +73,62 @@ def create_ts_df(throw_list, columns):
 
 
 def classify_ts(X_train, X_test, y_train, y_test):
-    scaler = TimeSeriesScalerMeanVariance()
-    X_train = scaler.fit_transform(X_train)
-    X_test = scaler.transform(X_test)
+    # scaler = TimeSeriesScalerMeanVariance()
+    # X_train = scaler.fit_transform(X_train)
+    # X_test = scaler.transform(X_test)
 
-    knn = KNeighborsTimeSeriesClassifier(n_neighbors=2)
-    p_grid = {"n_neighbors": [1, 5]}
-    cv = KFold(n_splits=5, shuffle=True, random_state=SEED)
-    clf = GridSearchCV(estimator=knn, param_grid=p_grid, cv=cv)
-
-    cv_scores = cross_val_score(clf, X_train, y_train, cv=cv, scoring="accuracy")
-    print("\nAccuracy of cross-validation (Time Series): ", cv_scores)
-    print("Mean cross-validation accuracy: ", np.mean(cv_scores))
-
+    clf = BaggingClassifier(
+        RocketClassifier(num_kernels=100),
+        n_estimators=10,
+    )
     clf.fit(X_train, y_train)
     print("Predicted labels: " + str(clf.predict(X_test)))
     print("True labels: " + str(y_test))
+
+    # knn = KNeighborsTimeSeriesClassifier(n_neighbors=2)
+    # p_grid = {"n_neighbors": [1, 5]}
+    # cv = KFold(n_splits=5, shuffle=True, random_state=SEED)
+    # clf = GridSearchCV(estimator=knn, param_grid=p_grid, cv=cv)
+
+    # cv_scores = cross_val_score(clf, X_train, y_train, cv=cv, scoring="accuracy")
+    # print("\nAccuracy of cross-validation (Time Series): ", cv_scores)
+    # print("Mean cross-validation accuracy: ", np.mean(cv_scores))
+
+    # clf.fit(X_train, y_train)
+    # print("Predicted labels: " + str(clf.predict(X_test)))
+    # print("True labels: " + str(y_test))
+
+
+def evaluate_classifiers(X, y):
+    classifiers = {
+        "Rocket + Bagging": BaggingClassifier(
+            RocketClassifier(num_kernels=100), n_estimators=10
+        ),
+        "TimeSeriesForest": TimeSeriesForestClassifier(n_estimators=100),
+        # Shapelet Transform": ShapeletTransformClassifier(),
+    }
+
+    results = []
+
+    for name, clf in classifiers.items():
+        print(name)
+        kf = KFold(n_splits=5, shuffle=True, random_state=SEED)
+        accuracy = cross_val_score(clf, X, y, cv=kf, scoring="accuracy")
+        precision = cross_val_score(clf, X, y, cv=kf, scoring="precision_weighted")
+        recall = cross_val_score(clf, X, y, cv=kf, scoring="recall_weighted")
+        f1 = cross_val_score(clf, X, y, cv=kf, scoring="f1_weighted")
+
+        results.append(
+            {
+                "Classifier": name,
+                "Accuracy": np.mean(accuracy),
+                "Precision": np.mean(precision),
+                "Recall": np.mean(recall),
+                "F1 Score": np.mean(f1),
+            }
+        )
+
+    return pd.DataFrame(results)
 
 
 def classify_features(X_train, X_test, y_train, y_test):
@@ -106,17 +154,17 @@ if __name__ == "__main__":
     throws_list = read_csv_throws()
     labels = [throw["Label"].iloc[0] for throw in throws_list]
 
-    throw_features = feature_extraction(throws_list)
+    # throw_features = feature_extraction(throws_list)
 
-    X_train_feat, X_test_feat, y_train_feat, y_test_feat = train_test_split(
-        throw_features,
-        labels,
-        stratify=labels,
-        test_size=0.2,
-        random_state=SEED,
-    )
+    # X_train_feat, X_test_feat, y_train_feat, y_test_feat = train_test_split(
+    #     throw_features,
+    #     labels,
+    #     stratify=labels,
+    #     test_size=0.2,
+    #     random_state=SEED,
+    # )
 
-    classify_features(X_train_feat, X_test_feat, y_train_feat, y_test_feat)
+    # classify_features(X_train_feat, X_test_feat, y_train_feat, y_test_feat)
 
     throw_df = create_ts_df(
         throws_list,
@@ -139,4 +187,12 @@ if __name__ == "__main__":
         random_state=SEED,
     )
 
-    classify_ts(np.array(X_train_raw), np.array(X_test_raw), y_train_raw, y_test_raw)
+    # classify_ts(
+    #     np.array(X_train_raw),
+    #     np.array(X_test_raw),
+    #     np.array(y_train_raw),
+    #     np.array(y_test_raw),
+    # )
+
+    df = evaluate_classifiers(np.array(X_train_raw), np.array(y_train_raw))
+    print(df)
